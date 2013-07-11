6ad8e0200887d6e72105605c4b88c153d52ca0f9
Ubuntu <ubuntu@ip-172-31-4-44.us-west-2.compute.internal>
2013-07-11 01:00:35 +0000

    Modified string from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var strout = new Buffer(readFileSync('index.html'));
+// var buf = new Buffer(strout)
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(strout.toString());
 });
 
 var port = process.env.PORT || 5000;
